46e533bf4d8e3fe6ef11e34db29ec3ef13cd2dff
717kb <139049983+717kb@users.noreply.github.com>
2026-09-05 13:47:47 +0200

    chore(branding): rename platform and repository paths to TIkZfy

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://antoniomachuca.github.io',
-  base: '/TikZFfy/',
+  base: '/TIkZfy/',
   vite: {
     plugins: [tailwindcss()],
     server: {
